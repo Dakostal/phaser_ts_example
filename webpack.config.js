2760569fd579ec35c743bc5fba4fed1c9a0914f6
Ilya Smakovskiy <96537307+WebSmaIL@users.noreply.github.com>
2023-03-02 00:01:07 +0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,21 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
-  entry: "./src/app.ts",
+  entry: './src/app.ts',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: [ '.ts', '.tsx', '.js' ]
   },
   output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "./public_html/js"),
+    filename: 'app.js',
+    path: path.resolve(__dirname, './public_html/js')
   },
-  mode: "development",
-  devServer: {
-    devMiddleware: {
-      mimeTypes: { phtml: 'text/html' },
-    },
-    static: {
-      directory: path.join(__dirname, "public_html"),
-    },
-    compress: true,
-  },
+  mode: 'development'
 };
